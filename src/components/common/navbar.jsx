350fe86bf269d559f294f6b065289a5122e6177c
Sheena Gupta <sheena.gupta.in@gmail.com>
2019-05-06 12:34:08 -0700

    navbar

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from "react";
 import "../../css/navbar.css";
 import { Redirect } from "react-router";
+import { If } from "react-if";
 
 class Navbar extends Component {
+  handleLogout = () => {
+    localStorage.removeItem("id");
+    localStorage.removeItem("username");
+    console.log("Logged out successfully.");
+    return <Redirect to="/home" />;
+  };
   render() {
-    let redirectVar = null;
     var id = localStorage.getItem("id");
-    if (!id) {
-      redirectVar = <Redirect to="/home" />;
-    }
+
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light ">
-          <a className="navbar-home" href="/home">
-            OpenHack
-          </a>
+        <If condition={!id}>
+          <nav className="navbar navbar-expand-lg navbar-light ">
+            <a className="navbar-home" href="/home">
+              OpenHack
+            </a>
 
-          <a href="/login" className="login">
-            Login
-          </a>
-        </nav>
+            <a href="/login" className="login">
+              Login
+            </a>
+          </nav>
+        </If>
+        <If condition={id}>
+          <nav className="navbar navbar-expand-lg navbar-light ">
+            <a className="navbar-home" href="/home">
+              OpenHack
+            </a>
+
+            <div
+              className="dropdown-menu dropdown-content"
+              aria-labelledby="navbarDropdown"
+            >
+              <a className="dropdown-item" href="/profile">
+                Profile
+              </a>
+              <a className="dropdown-item" href="/organizations">
+                My Organizations
+              </a>
+              <a className="dropdown-item" onClick={this.handleLogout}>
+                Logout
+              </a>
+            </div>
+          </nav>
+        </If>
       </div>
     );
   }
