6a2bf1bd1b740e4761cb6b82e258e04a731f9b66
Sheena Gupta <sheena.gupta.in@gmail.com>
2019-05-08 04:44:19 -0700

    hackathons token

--- src/components/hackathons/adminHackathons.jsx
+++ src/components/hackathons/adminHackathons.jsx
@@ -5,107 +5,125 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Form from "../common/form";
 import { Redirect } from "react-router";
-import { If } from "react-if";
+import { If, Then, Else } from "react-if";
 import { paginate } from "../utils/paginate";
 import Pagination from "../common/pagination";
+import {
+  getToken,
+  getJWTUsername,
+  getJWTID,
+  getJWTScreenName,
+  getJWTAdminStatus,
+  setHeader
+} from "../common/auth";
 
 class AdminHackathons extends Form {
-    constructor() {
-        super();
-        this.state = {
-            hackathons: [],
-            data: {},
-            isAdmin: false,
-            currentPage: 1,
-            pageSize: 8
-        };
-    }
-
-    componentDidMount() {
-        const username = localStorage.getItem("username");
-        if (username.includes("@sjsu.edu")) this.setState({ isAdmin: true });
-
-        axios.get("http://localhost:8080/hackathons/my").then(response => {
-            console.log(response.data);
-            this.setState({
-                hackathons: response.data
-            });
-        });
-    }
-
-    handlePageChange = page => {
-        this.setState({ currentPage: page });
+  constructor() {
+    super();
+    this.state = {
+      hackathons: [],
+      data: {},
+      isAdmin: false,
+      currentPage: 1,
+      pageSize: 8
     };
+  }
 
-    render() {
-        let redirectVar = null;
-        var id = localStorage.getItem("id");
-        if (!id) {
-            redirectVar = <Redirect to="/home" />;
-        }
+  componentDidMount() {
+    if (getJWTAdminStatus()) this.setState({ isAdmin: true });
 
-        const paginatedData = paginate(
-            this.state.hackathons ? this.state.hackathons : "",
-            this.state.currentPage,
-            this.state.pageSize
-        );
-        return (
-            <div className="hack-home">
-                {redirectVar}
-                <Navbar />
-                <h1>My PERSONAL HACAKTHONS SPACE</h1>
-                <div className="heading">
-                    Find, compete, and earn points for your school at the largest, most
-                    diverse
-                    <br /> <span>student hackathons in the world.</span>
-                </div>
-                <If condition={sessionStorage.getItem('isAdmin')}>
-                    <a className="btn btn-primary create-hack" href="/hackathon/create">
-                        Create Hackathon
-                    </a>
-                </If>
+    axios.get("http://localhost:8080/hackathons/my").then(response => {
+      console.log(response.data);
+      this.setState({
+        hackathons: response.data
+      });
+    });
+  }
 
-                {paginatedData.map(hackathon => (
-                    <div className="hackathons-list">
-                        <div className="hackathon">
-                            <img
-                                src={require("../../images/9.jpg")}
-                                width="250"
-                                height="250"
-                            />
-                            <div className="hackathon-info">
-                                <h3>
-                                    <Link
-                                        className="link"
-                                        hack_id = {this.state.hackathons}
-                                        to={{
-                                            pathname: "/hackathons/adminHackathons/hackathon-view",
-                                            state: { id: hackathon.id }
-                                        }}
-                                        params={{testvalue:"hello"}}
-                                    >
-                                        {hackathon.name}
-                                    </Link>
-                                </h3>
-                                {hackathon.startDate} - {hackathon.endDate}
-                                <br />
-                                {hackathon.status}
-                            </div>
-                        </div>
-                    </div>
-                ))}
-                <div className="general_pagination">
-                    <Pagination
-                        itemsCount={
-                            this.state.hackathons ? this.state.hackathons.length : ""
-                        }
-                        pageSize={this.state.pageSize}
-                        onPageChange={this.handlePageChange}
-                        currentPage={this.state.currentPage}
-                    />
-                </div>
-            </div>
-        );
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    let redirectVar = null;
+    var id = getJWTID();
+    if (!id) {
+      redirectVar = <Redirect to="/home" />;
     }
+
+    const paginatedData = paginate(
+      this.state.hackathons ? this.state.hackathons : "",
+      this.state.currentPage,
+      this.state.pageSize
+    );
+    return (
+      <div className="hack-home">
+        {redirectVar}
+        <Navbar />
+        <h1>My Hackathons</h1>
+        <div className="heading">
+          Find, compete, and earn points for your school at the largest, most
+          diverse
+          <br /> <span>student hackathons in the world.</span>
+        </div>
+        <If condition={getJWTAdminStatus()}>
+          <a className="btn btn-primary create-hack" href="/hackathon/create">
+            Create Hackathon
+          </a>
+        </If>
+
+        <If
+          condition={this.state.hackathons && this.state.hackathons.length != 0}
+        >
+          <Then>
+            {paginatedData.map(hackathon => (
+              <div className="hackathons-list">
+                <div className="hackathon">
+                  <img
+                    src={require("../../images/9.jpg")}
+                    width="250"
+                    height="250"
+                  />
+                  <div className="hackathon-info">
+                    <h3>
+                      <Link
+                        className="link"
+                        hack_id={this.state.hackathons}
+                        to={{
+                          pathname:
+                            "/hackathons/adminHackathons/hackathon-view",
+                          state: { id: hackathon.id }
+                        }}
+                        params={{ testvalue: "hello" }}
+                      >
+                        {hackathon.name}
+                      </Link>
+                    </h3>
+                    {hackathon.startDate} - {hackathon.endDate}
+                    <br />
+                    {hackathon.status}
+                  </div>
+                </div>
+              </div>
+            ))}
+            <div className="general_pagination">
+              <Pagination
+                itemsCount={
+                  this.state.hackathons ? this.state.hackathons.length : ""
+                }
+                pageSize={this.state.pageSize}
+                onPageChange={this.handlePageChange}
+                currentPage={this.state.currentPage}
+              />
+            </div>
+          </Then>
+          <Else>
+            {" "}
+            <div>no</div>
+          </Else>
+        </If>
+      </div>
+    );
+  }
 }
 export default AdminHackathons;
--- src/components/hackathons/hackathon.jsx
+++ src/components/hackathons/hackathon.jsx
@@ -5,6 +5,14 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Redirect } from "react-router";
 import Form from "../common/form";
+import {
+  getToken,
+  getJWTUsername,
+  getJWTID,
+  getJWTScreenName,
+  getJWTAdminStatus,
+  setHeader
+} from "../common/auth";
 
 class Hackathon extends Component {
   constructor() {
@@ -16,14 +24,14 @@ class Hackathon extends Component {
   }
 
   componentDidMount() {
-    const ID = this.props.location.state.id;
-    console.log("id=", ID);
+    const ID = this.props.location.state ? this.props.location.state.id : "";
+    setHeader();
     axios.get("http://localhost:8080/hackathons/" + ID).then(response => {
       this.setState({
         hackathon: response.data
       });
     });
-
+    setHeader();
     axios
       .get("http://localhost:8080/hackathons/" + ID + "/teams")
       .then(response => {
@@ -35,7 +43,7 @@ class Hackathon extends Component {
 
   render() {
     let redirectVar = null;
-    var id = localStorage.getItem("id");
+    var id = getJWTID();
     if (!id) {
       redirectVar = <Redirect to="/home" />;
     }
@@ -52,59 +60,55 @@ class Hackathon extends Component {
           <br />
           <label> Start Date:</label>
           <input
-              type="date"
-              name="startDate"
-              value={this.state.hackathon ? this.state.hackathon.startDate : ""}
-              className="form-control"
-              disabled
+            type="date"
+            name="startDate"
+            value={this.state.hackathon ? this.state.hackathon.startDate : ""}
+            className="form-control"
+            disabled
           />
           <label> End Date:</label>
-          <br/>
+          <br />
           <input
-              type="date"
-              name="endDate"
-              value={this.state.hackathon ? this.state.hackathon.endDate : ""}
-              className="form-control"
-              disabled
+            type="date"
+            name="endDate"
+            value={this.state.hackathon ? this.state.hackathon.endDate : ""}
+            className="form-control"
+            disabled
           />
           <label>Fee:</label>
           <input
-              type="text"
-              name="Fee"
-              value={this.state.hackathon ? this.state.hackathon.fee : ""}
-              className="form-control"
-              disabled
+            type="text"
+            name="Fee"
+            value={this.state.hackathon ? this.state.hackathon.fee : ""}
+            className="form-control"
+            disabled
           />
-          <label>Team Size:</label><br/>
+          <label>Team Size:</label>
+          <br />
           Minimum: &nbsp;
           <input
-              type="text"
-              name="Min"
-              value={this.state.hackathon
-                  ? this.state.hackathon.minSize
-                  : ""}
-              className="form-control"
-              disabled
+            type="text"
+            name="Min"
+            value={this.state.hackathon ? this.state.hackathon.minSize : ""}
+            className="form-control"
+            disabled
           />
           Maximum: &nbsp;
           <input
-              type="text"
-              name="Max"
-              value={this.state.hackathon
-                  ? this.state.hackathon.maxSize
-                  : ""}
-              className="form-control"
-              disabled
+            type="text"
+            name="Max"
+            value={this.state.hackathon ? this.state.hackathon.maxSize : ""}
+            className="form-control"
+            disabled
           />
-          <label>Status:</label><br/>
+          <label>Status:</label>
+          <br />
           <input
-              type="text"
-              name="Status"
-              value={this.state.hackathon
-                  ? this.state.hackathon.status
-                  : ""}
-              className="form-control"
-              disabled
+            type="text"
+            name="Status"
+            value={this.state.hackathon ? this.state.hackathon.status : ""}
+            className="form-control"
+            disabled
           />
         </div>
         <div className="hackathon-team">
