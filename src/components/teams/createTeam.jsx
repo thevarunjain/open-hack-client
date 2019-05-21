77fd2f4da4549f1b471b5573db6c1ba1e8d61762
Varun Jain <varunsj18@gmail.com>
2019-05-20 22:31:38 -0700

    create team

--- src/components/teams/createTeam.jsx
+++ src/components/teams/createTeam.jsx
@@ -30,32 +30,108 @@ class CreateTeam extends Form {
       roles: []
     };
 
-    var membersName = this.state.data.members
-      ? this.state.data.members.split(";").map(e => e.trim())
-      : [];
-    membersName.map(async (name, i) => {
-      if (name.replace(/\s/gi, "").length != 0) {
-        setHeader();
-        var res = await axios.get(rootUrl+"/users?name=" + name);
-        var jid = Number.parseInt(res.data[0].id, 10) - 1;
-        data["members"].push(jid + 1);
-      }
-    });
+    
+  handleSponsorSelection = (selectedOption,i) => {
+    var sponsorById=this.state.x;
 
-    var rolesArray = this.state.data.roles
-      ? this.state.data.roles.split(";")
-      : [];
+    if(sponsorById && !sponsorById.includes(selectedOption.id)){    // for unique insertion in x object
+          sponsorById[i]=selectedOption.id;
+          this.setState({ selectedOption });
+          this.setState({ x : sponsorById });
+    }else if(this.state.x && this.state.x.includes(selectedOption.id)){
+      window.alert("already added")
+          this.state.sponsors.splice(i,1);        // to remove the <div>
+          this.setState({sponsors: this.state.sponsors})
+      
+          this.state.discounts.splice(i,1);       //to remove its discount    
+          this.setState({discounts: this.state.discounts})
+      
+          this.state.x.splice(i,1);               // to remove as a sponsor
+          this.setState({x: this.state.x})
+    }
+  }
+  
+  
+  addSponsor(e){
+    e.preventDefault();
+    this.setState({
+      sponsors : [...this.state.sponsors, ""]
+    })
+  }
 
-    data["roles"] = rolesArray.map(e => e.trim());
+  removeSponsor(e,i){
+    e.preventDefault();
+    
+    this.state.sponsors.splice(i,1);
+    this.setState({sponsors: this.state.sponsors})
 
-    this.setState(
-      {
-        dataSend: data
-      },
-      function() {
-        console.log(this.state.dataSend);
-      }
-    );
+    this.state.discounts.splice(i,1);
+    this.setState({discounts: this.state.discounts})
+
+    this.state.x.splice(i,1);
+    this.setState({x: this.state.x})
+
+    console.log(this.state);
+  }
+  
+  componentDidMount(){
+    setHeader();
+    axios.get(rootUrl + "/users")
+    .then(users=>{
+      // remove isadmin true users.
+      var nonAdminUsers = users.data.filter(e=> !e.admin);
+
+      nonAdminUsers.map(e=>{
+        e["label"] = e.name.first+ " "+e.name.last;
+        e["value"] = e.id
+      })
+      this.setState({
+        allJudges : nonAdminUsers
+      })
+    }).catch(err=>{
+
+    })
+
+    axios.get(rootUrl + "/organizations").then(org=>{
+      org.data.map(e=>{
+        e["label"] = e.name;
+        e["value"] = e.id
+      })
+      this.setState({
+        allSponsors : org.data
+      })
+    })
+  }
+
+
+
+
+    // var membersName = this.state.data.members
+    //   ? this.state.data.members.split(";").map(e => e.trim())
+    //   : [];
+    // membersName.map(async (name, i) => {
+    //   if (name.replace(/\s/gi, "").length != 0) {
+    //     setHeader();
+    //     var res = await axios.get(rootUrl+"/users?name=" + name);
+    //     var jid = Number.parseInt(res.data[0].id, 10) - 1;
+    //     data["members"].push(jid + 1);
+    //   }
+    // });
+
+    // var rolesArray = this.state.data.roles
+    //   ? this.state.data.roles.split(";")
+    //   : [];
+
+    // data["roles"] = rolesArray.map(e => e.trim());
+
+    // this.setState(
+    //   {
+    //     dataSend: data
+    //   },
+    //   function() {
+    //     console.log(this.state.dataSend);
+    //   }
+    // );
   };
 
   async submit(e) {
@@ -77,6 +153,7 @@ class CreateTeam extends Form {
         this.props.history.push("/hackathons");
       });
   }
+
   render() {
     let redirectVar = null;
     var id = getJWTID();
@@ -104,7 +181,30 @@ class CreateTeam extends Form {
                 onChange={this.handleChange}
               />
               <label>Add Members to team</label>
-              <input
+              
+              {
+                this.state.members.map((sponsor,i)=>{
+                  return(
+                    <div class="input-group mb-3" key={i}>
+                      <Select key={i} className="css-1pcexqc-container-spon"  value={this.state.x[i] ? this.state.x[i].name : ""}  options={this.state.allMember.filter((e) => !this.state.x.includes(e.id))} onChange={(e)=>{this.handleMemberSelection(e,i)}} /> 
+                       <input 
+                        required
+                        className="form-control"
+                        placeholder="discount"
+                        style={{marginLeft: "12px", borderRadius: "4px", height: "59px",  marginTop: "0px"}}
+                      onChange={(e)=>this.handleDiscountChange(e,i)}
+                      value ={this.state.discounts[i]}
+                      />
+                    <div class="input-group-append" style={{    marginTop: "-11px"}}>  
+                      <button class="btn btn-primary btn-remove" onClick={(e)=>this.removeMember(e,i)}>Remove</button>
+                    </div>
+                    </div>
+                  )
+                })
+              }
+              <br/>
+              <button className="btn-add" onClick={(e)=>this.addMember(e)}>Add Sponsor </button>
+              {/* <input
                 type="text"
                 name="members"
                 className="form-control"
@@ -118,7 +218,7 @@ class CreateTeam extends Form {
                 className="form-control"
                 placeholder="Semi-colon(;) separated"
                 onChange={this.handleChange}
-              />
+              /> */}
             </div>
             <div>
               <button
